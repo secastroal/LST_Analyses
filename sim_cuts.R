7ee34d4fc9915aefa5926180b022c923449d54aa
S. Castro Alvarez <P280329@WORKSPACE.RUG.NL>
2019-01-11 17:02:39 +0100

    function sim cuts finished

--- sim_cuts.R
+++ sim_cuts.R
@@ -1,4 +1,5 @@
 # Prepare environment---
+rm(list=ls())
 library(invgamma)
 
 # function to simulate msst data ----
@@ -36,7 +37,7 @@ sd_utrait <- sqrt(var_utrait) # sd latent  common trait
 
 # Data simulation ----
 
-ctrait_scores <- rnorm(N, 0, sd_trait) # common trait scores
+ctrait_scores <- rnorm(N, 0, sd_ctrait) # common trait scores
 ctrait_scores_full <- matrix(rep(ctrait_scores, each = nT), nrow = N * nT, ncol = I, byrow = FALSE) # matrix with common trait scores
 
 # unique trait scores
@@ -48,21 +49,22 @@ rm(i)
 utrait_scores_full <- utrait_scores[rep(1:N, each = nT), ]
 
 
-state_scores <- rnorm(N * nT, 0, sd_state) # factor state scores
-state_scores_full <- matrix(state_scores, nrow = N * nT, ncol = I , byrow = FALSE) # full matrix with factor state scores
+cstate_scores <- rnorm(N * nT, 0, sd_cstate) # common state scores
+cstate_scores_full <- matrix(cstate_scores, nrow = N * nT, ncol = I , byrow = FALSE) # full matrix with common state scores
 
-# measurement errors
-errors <- matrix(NA, N * nT, I)
+# unique state or measurement error
+ustate_scores_full <- matrix(NA, N * nT, I)
 for(i in 1:I){
-  errors[,i] <- rnorm(N * nT, 0, sd_m_error[i])
+  ustate_scores_full[,i] <- rnorm(N * nT, 0, sd_ustate[i])
 }
 rm(i)
 
 # Complete data
 sim_data <- matrix(int, nrow = N *nT, ncol = I, byrow = TRUE) + # intercepts
-  trait_scores_full * matrix(lambda_trait, nrow = N *nT, ncol = I, byrow = TRUE) + # trait scores times trait lambdas
-  state_scores_full * matrix(lambda_state, nrow = N *nT, ncol = I, byrow = TRUE) + # state scores times state lambdas
-  errors # errors
+  ctrait_scores_full * matrix(lambda_ctrait, nrow = N *nT, ncol = I, byrow = TRUE) + # trait scores times trait lambdas
+  utrait_scores_full + # unique trait scores 
+  cstate_scores_full * matrix(lambda_cstate, nrow = N *nT, ncol = I, byrow = TRUE) + # state scores times state lambdas
+  ustate_scores_full # unique states
 
 sim_data <- data.frame(cbind(rep(1:N, each = nT), sim_data), row.names = NULL)
 
@@ -71,7 +73,7 @@ colnames(sim_data) <- c("subjn", paste0("y", 1:I))
 
 
 # model estimation ----
-file.name <- "sim_msst_test"
+file.name <- "sim_cuts_test"
 prepareMplusData(sim_data, paste0("ML_Mplus_files/",file.name,".dat"), inpfile = T)
 
 analysis_syntax <- "USEVAR = y1 y2 y3 y4;
@@ -82,7 +84,7 @@ TYPE = TWOLEVEL;
 ESTIMATOR = ML;
 ITERATIONS = 500000;" # increase H1 iterations
 
-ml_syntax <- write.mlmsst.to.Mplus(sim_data[, -1])
+ml_syntax <- write.mlcuts.to.Mplus(sim_data[, -1])
 
 write(analysis_syntax, paste0("ML_Mplus_files/",file.name,".inp"), append = T) # Write Analysis specifications
 write(ml_syntax, paste0("ML_Mplus_files/",file.name,".inp"), append = T)
