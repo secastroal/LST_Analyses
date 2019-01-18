5c572296a9f67dca5af9edf6703ea341e5690e07
S. Castro Alvarez <P280329@WORKSPACE.RUG.NL>
2019-01-18 14:48:28 +0100

    Two R files where created to storage the different functions. One file contains all the functions that allow writting Mplus sintaxes. The second one includes the functions that allow simulating data based on the three LST models.

---
+++ LSTSimulation.R
@@ -0,0 +1,11 @@
+## This document contains functions that simulate data of three LST models.
+
+# Contents
+# 1.0 sim.data.msst
+# 2.0 sim.data.cuts
+# 3.0 sim.data.tso
+
+
+# 1.0 sim.data.msst ----
+# 2.0 sim.data.cuts ---- 
+# 3.0 sim.data.tso ----
